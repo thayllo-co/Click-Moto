4fb565e27583ab7502ceae6e0c18b30c7cf08306
Thayllo Oliveira <thayllo.co@gmail.com>
2023-01-25 17:38:08 -0300

    React Native setup for Android and iOS

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Edit me! 🎉</Text>
+    </View>
+  );
+}
+
+export default App;
